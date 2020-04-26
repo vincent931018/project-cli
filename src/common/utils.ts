73ae52067e26eee3edfb0962e5d8ddc09350bc3f
曹文成 <845982120@qq.com>
2020-04-26 16:08:48 +0800

    fix: log color

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,9 +1,9 @@
 const chalk = require("chalk");
-const log = console.log;
 
 export const timeLog = (logInfo: string) => {
     const nowDate = new Date();
-    log(
-        chalk.green(`[${nowDate.toLocaleString()}.${nowDate.getMilliseconds().toString().padStart(3, "0")}] ${logInfo}`)
+    /* eslint-disable no-console */
+    console.log(
+        `[${nowDate.toLocaleString()}.${nowDate.getMilliseconds().toString().padStart(3, "0")}] ${chalk.green(logInfo)}`
     );
 };
